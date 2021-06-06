6a4d54c0d3d463700aaff336fadb8c7c7a8089c9
nao0405 <80329337+nao0405@users.noreply.github.com>
2021-06-06 09:47:57 +0900

    課題提出用
    データ型・文字クラスの課題

--- JavaStudy/1-1/Task1_1.java
+++ JavaStudy/1-1/Task1_1.java
@@ -16,7 +16,7 @@ public class Task1_1 {
         // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
         String hogeString = "テスト";
         // ③下記の処理について、何をしているのかコメントを記入してください
-        // [dobule型の hogeDouble という変数を宣言し、値｢0.2｣で初期化]
+        // [double型の hogeDouble という変数を宣言し、値｢0.2｣で初期化]
         double hogeDouble = 0.2;
 
         // ④変数 hogeInt を値「100」で上書き
