04d816f23183655e3fd9dd7331d808f3fc471841
nao0405 <80329337+nao0405@users.noreply.github.com>
2021-06-13 11:48:13 +0900

    提出課題
    メソッド

--- JavaStudy/2-1/src/study/Main.java
+++ JavaStudy/2-1/src/study/Main.java
@@ -24,12 +24,9 @@ public class Main {
 
 		//③ arr 配列をループさせ、isOdd関数を使って配列の要素が奇数かどうかの判定を行いなさい。
 		//   要素が奇数の場合には「nは奇数です。」と出力されます。
-		for (int isOdd = 0; isOdd < arr.length; isOdd++) {
-			if (arr[isOdd] % 2 != 0) {
-				System.out.println(arr[isOdd] + "は奇数です。");
-			}
-		}
 
+		for (int i = 0; i < arr.length; i++, isOdd(arr[i-1])) {
+		}
 	}
 
 	//② 関数を定義しなさい。
