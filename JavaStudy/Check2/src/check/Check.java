e4688d2c460b2cd58c2704b811489b8220dd9b1e
nao0405 <80329337+nao0405@users.noreply.github.com>
2021-07-03 07:23:04 +0900

    提出課題
    2章チェックテスト

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -5,13 +5,13 @@ import constants.Constants;
 public class Check {
 
 	// フィールド変数
-	private static String firstName = "有田";
-	private static String lastName = "尚史";
+	private static String firstName;
+	private static String lastName;
 
 	public static void main(String[] args) {
 
 		//インスタンス	
-		Check check = new Check(firstName, lastName);
+		Check check = new Check("有田","尚史");
 		check.printName();
 
 		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
